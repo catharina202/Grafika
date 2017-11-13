235e45c9321be7ac8f9a2f945e70f7cf11dbabd7
Katarzyna Krzemień <catharina202@gmail.com>
2017-11-13 14:56:00 +0100

    zajecia 1 end

---
+++ ZmienNazwe/Okno.cpp
@@ -0,0 +1,37 @@
+#include "Okno.h"
+int main(int argc, char** argv)
+{
+	Okno okno(786, 1024, 100, 100);
+	okno.stowrzenieOkna(argc, argv);
+	glutDisplayFunc(okno.wyswietl);
+	glClearColor(1.0f, 0.8f, 0.9f, 0.0f);
+	glutMainLoop();
+
+	return 0;
+}
+Okno::Okno(int wysokoscOkna, int szerokoscOkna, int polozenieOknaX, int polozenieOknaY){
+	this->wysokoscOkna = wysokoscOkna;
+	this->szerokoscOkna = szerokoscOkna;
+	this->polozenieOknaX = polozenieOknaX;
+	this->polozenieOknaY = polozenieOknaY;
+}
+
+Okno::Okno(){
+	wysokoscOkna = 786;
+	szerokoscOkna = 1024;
+	polozenieOknaX = 100;
+	polozenieOknaY = 100;
+}
+void Okno::stowrzenieOkna(int argc, char** argv){
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1024, 786);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("okno");
+
+}
+
+void Okno::wyswietl(){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
