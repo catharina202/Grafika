72e197e35326f8f59e0106215fd50459ad997b94
catharina202 <33626765+catharina202@users.noreply.github.com>
2018-01-20 11:53:28 +0100

    OKNO

---
+++ Okno/Okno.cpp
@@ -0,0 +1,55 @@
+#include "Okno.h"
+
+int main(int argc, CHAR* argv[])
+{
+	Okno okno(786, 1024, 100, 100);
+	okno.stworzenieOkna(argc, argv);
+	glutDisplayFunc(okno.wyswietl);
+
+	glClearColor(1.0f, 0.9f, 0.2f, 0.0f);
+
+	glutMainLoop();
+
+	return 0;
+}
+
+int Okno::wysokoscOkna;
+int Okno::szerokoscOkna;
+
+
+
+Okno::Okno(int wysokoscOkna, int szerokoscOkna, int polozenieOknaX, int polozenieOknaY)
+{
+	this->wysokoscOkna = wysokoscOkna;
+	this->szerokoscOkna = szerokoscOkna;
+	this->polozenieOknaX = polozenieOknaX;
+	this->polozenieOknaY = polozenieOknaY;
+}
+
+Okno::Okno(void)
+{
+	wysokoscOkna = 768;
+	szerokoscOkna = 1024;
+	polozenieOknaX = 100;
+	polozenieOknaY = 100;
+}
+
+void Okno::stworzenieOkna(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1024, 768);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("okno");
+}
+
+void Okno::wyswietl()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+Okno::~Okno()
+{
+
+}
